e64b061ab82b2117cb2ee8a777b0f1d5731297ed
thallissousa <thallissousa@outlook.com>
2021-11-04 17:14:38 -0300

    Alteração nas cores e no alinhamento dos textos

--- SectorExample/SectorExample/ContentView.swift
+++ SectorExample/SectorExample/ContentView.swift
@@ -12,17 +12,24 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                VStack(alignment: .center) {
-                    Text("Gráfico de Setores")
-                        .font(.title) //bold
+                VStack(alignment: .leading){
+                Text("Gráfico de Setores").font(.title2)
                         .padding()
-                 PieChart(data: [0.27, 0.15, 0.18, 0.40], cor: [.red, .blue, .green, .purple ], legenda: ["Agricul.", "Transp.", "Indust.", "Energia "])
+                    
+                    VStack(alignment: .center) {
+                    PieChart(
+                        data: [0.25, 0.25, 0.25, 0.25],
+                        cor: [Color(red: 245 / 255, green: 69 / 255, blue: 79 / 255), Color(red: 146 / 255, green: 188 / 255, blue: 117 / 255), Color(red: 244 / 255, green: 150 / 255, blue: 63 / 255), Color(red: 89 / 255, green: 119 / 255, blue: 142 / 255)
+                        ],
+                        legenda: ["Ex.1", "Ex.2", "Ex.3", "Ex.4"])
                         .frame(height: 300)
+                        .padding()
                     Text("Um gráfico de setores (também conhecido como gráfico de pizza) compara as partes com um todo. Como tal, mostra uma distribuição percentual. A pizza representa o conjunto de dados total e cada segmento da pizza é uma categoria específica dentro do todo.")
                         .padding()
                         .font(.headline)
 
                 }
+                
             } .navigationTitle("Setores")
         }
     }
@@ -33,4 +40,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
