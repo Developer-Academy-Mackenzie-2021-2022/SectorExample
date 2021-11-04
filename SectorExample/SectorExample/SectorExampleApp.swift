b876546116651184c493df856324f666428c03a8
thallissousa <thallissousa@outlook.com>
2021-11-04 13:26:17 -0300

    Sector Example - App

---
+++ SectorExample/SectorExample/SectorExampleApp.swift
@@ -0,0 +1,22 @@
+//
+//  SectorExampleApp.swift
+//  SectorExample
+//
+//  Created by Thallis Sousa on 04/11/21.
+//
+
+import SwiftUI
+import FrameworkDeGraficos
+
+@main
+struct SectorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+            AreaGraph()
+            HeatmapGraph()
+        
+        }
+    }
+}
